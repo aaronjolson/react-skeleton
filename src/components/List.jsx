243f646064263e64c097911874e47785c95150cd
aaron <aaolson817@gmail.com>
2015-12-27 17:10:42 -0600

    react skeleton

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -7,8 +7,11 @@ var List = React.createClass({
     render: function () {
         var listItems = ingredients.map(function (item) {
             return <ListItem key={item.id} ingredient={item.text} />;
-    });
-    return (<ul>{listItems}</ul>);
-  }
+        });
+        
+        return <ul>{listItems}</ul>;
+    }
 });
+
+module.exports = List;
         
